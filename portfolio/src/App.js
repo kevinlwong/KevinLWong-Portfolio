9e41b56fc2ac56ce86a83ac76b851c4edaefc8c8
Kevin Wong <kevinwong973@gmail.com>
2024-10-26 01:29:49 -0700

    edited spacing of text and margins/padding

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,44 +5,22 @@ import React from 'react';
 // import './utilities.css';
 import './styles.css';
 import Sidebar from './Sidebar';
+import profilePic from './assets/images/ProfilePicofKevinWong.jpg';
+import kevinArt from './assets/images/kevinart.JPG';
+import github from './assets/images/github.jpeg';
+import linkedin from './assets/images/linkedin.png';
+import twitter from './assets/images/bw twitter.jpeg';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <header className="App-header">
-        {/* Portfolio Header Section */}
-        <div className="container">
-          <nav className="flex items-center justify-between">
-            <div className="left flex justify-right">
-              {/* <div className="logo"> */}
-                {/* <img src="./images/bronco.jpg" width="50px" alt="logo" /> */}
-              {/* </div> */}
-              {/* <div>
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#projects">Projects</a>
-                <a href="#more">More</a>
-              </div> */}
-            </div>
-            {/* <div class="right"> */}
-                    {/* <button class="btn btn-primary">Contact</button>
-                    <a class="btn btn-primary" href="mailto:your-email@example.com">Contact Me</a> */}
-                    {/* <button class="btn btn-primary" */}
-                        {/* onclick="window.location.href='mailto:kevinwong973@gmail.com'">Contact Me</button> */}
-                {/* </div> */}
-
-            <div className="right">
-              {/* Add the CallButton component here */}
-              {/* <CallButton /> */}
-            </div>
-          </nav>
-        </div>
 
         {/* Hero Section */}
-        <div id="home" className="hero flex items-centre justify-between">
+        <div id="home" className="hero container flex items-centre justify-between">
           <div className="left flex-1 justify-center">
-            <img src="./images/ProfilePicofKevinWong.JPG" alt="Profile" />
+          <img src={profilePic} alt="Profile" />
           </div>
           <div className="right flex-1">
             <h1>Kevin Wong</h1>
@@ -66,7 +44,7 @@ function App() {
       <section id="about" className="about">
         <div className="container flex items-centre">
           <div className="left flex-1 justify-right">
-            <img src="./images/kevinart.JPG" width="500px" alt="profile pic" />
+            <img src={kevinArt} width="500px" alt="profile pic" />
           </div>
           <div className="right flex-1">
             <h1>
@@ -120,7 +98,7 @@ function App() {
         </div>
       </section>
 
-      {/* <section id="projects" class="services">
+       <section id="projects" class="services">
   <div class="container">
     <h1 class="services-head">Projects</h1>
     <p>Here is a compilation of some of my projects! Click them to learn more about each one!</p>
@@ -135,11 +113,11 @@ function App() {
             messages.
           </p>
         </div>
-      </a> */}
-      {/* <!-- Repeat for other projects, ensuring correct image paths --> */}
-    {/* </div>
+      </a> 
+      {/* /* Repeat for other projects, ensuring correct image paths */}
+     </div>
   </div>
-</section> */}
+</section> 
 
  <section id="more" class="more">
         <div class="container">
@@ -171,12 +149,24 @@ function App() {
       {/* Additional Sections (Projects, More, etc.) go here */}
       <div className="socials">
   <a href="https://github.com/kevinlwong">
-    <img src="./images/github.jpeg" width="40px" alt="GitHub" />
+    <img src={github} width="40px" alt="GitHub" />
   </a>
   <a href="https://www.linkedin.com/in/kevinlwong88/">
-    <img src="./images/linkedin.png" width="40px" alt="LinkedIn" />
+    <img src={linkedin} width="40px" alt="LinkedIn" />
   </a>
 </div>
+
+ {/* <div class="right"> */}
+                    {/* <button class="btn btn-primary">Contact</button>
+                    <a class="btn btn-primary" href="mailto:your-email@example.com">Contact Me</a> */}
+                    {/* <button class="btn btn-primary" */}
+                        {/* onclick="window.location.href='mailto:kevinwong973@gmail.com'">Contact Me</button> */}
+                {/* </div> */}
+
+                {/* <div className="right"> */}
+              {/* Add the CallButton component here */}
+              {/* <CallButton /> */}
+            {/* </div> */}
       
     </div>
   );
